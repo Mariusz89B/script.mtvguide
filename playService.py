6ae6a6454b544e1d095446bba0d28084c93281e5
Mariusz89B <mariusz89b@mods-kodi.pl>
2021-06-03 21:48:45 +0200

    9.3.6_v12

--- playService.py
+++ playService.py
@@ -128,7 +128,6 @@ class BasePlayService:
         service = None
         try:
             params = url[8:].split('&')
-            deb('TEST: {}'.format(params))
             service = params[0]
             cid = params[1].split('=')[1]
             deb(self.__class__.__name__ + ' parseUrl: cid {}, service {}'.format(cid, service))
@@ -1485,7 +1484,6 @@ class PlayService(xbmc.Player, BasePlayService):
             elif status >= 300 and status < 400:
                 xbmcgui.Dialog().notification(strings(57058) + ' Status: ' + str(status), strings(31019), xbmcgui.NOTIFICATION_WARNING)
 
-            deb('TEST: {}'.format(status))
             return status
 
     def CheckInputstreamInstalledAndEnabled(self):
